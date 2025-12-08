86a4e8ab16bf0d0a1bcb6c964611eb8bde98ed13
Margo <djessi.real@gmail.com>
2025-12-08 19:57:03 +0300

    7-ая лаба

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,14 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-
     use HasFactory;
+
     protected $fillable = [
-    'title',
-    'shortDesc',
-    'desc',
-    'date',
-    'preview_image',
-    'full_image',
-];
+        'title',
+        'shortDesc',
+        'desc',
+        'date',
+        'preview_image',
+        'full_image',
+    ];
+
+    /**
+     * Связь: одна статья имеет много комментариев
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.app')
+
 @section('content')
     <h1>Новости</h1>
 
     @foreach($articles as $article)
-        <h3>{{ $article->title }}</h3>
+        <h3>
+            <a href="{{ route('news.show', $article->id) }}">
+                {{ $article->title }}
+            </a>
+        </h3>
         <p>{{ $article->shortDesc }}</p>
         <small>{{ $article->date }}</small>  
         <hr>
     @endforeach
+
     <div class="pagination">{{ $articles->links() }}</div>
-@endsection 
+@endsection
